3f6f0e52b18bd9bd4712bd0fc1e7d06a7a457716
Sergey Vetko <s.vetko@itgr.ru>
2018-09-07 14:11:22 +0300

    A small parser of dividends page on smart-lab. Gives back a json array

---
+++ web/dividends.php
@@ -0,0 +1,67 @@
+<?php
+
+require_once '../vendor/raveren/kint/Kint.class.php';
+
+$debugInfo = (boolean)(int)$_REQUEST['debugInfo'];
+if ($debugInfo) {
+	$s = !\Kint::dump(microtime(), 'started');
+}
+$URL = 'https://smart-lab.ru/dividends';
+$htmlContent = file_get_contents($URL);
+if ($debugInfo) {
+	$s = !\Kint::dump(microtime(), 'got content');
+}
+$result = preg_match_all(
+	/** @lang text */
+	"/<tr class=\"dividend_approved\"[^>]*>(\n?\r?.*?)<\/tr>/s", $htmlContent, $matches);
+if ($debugInfo) {
+	$s = !\Kint::dump(microtime(), 'got rows');
+}
+
+$resultsArr = [];
+$rowsStr = '';
+
+$neededColumns = [1 => 'Ticker', 4 => 'Date', 5 => 'Year', 6 => 'Period', 7 => 'Amount'];
+$resultsCounter = 0;
+foreach ((array)$matches[0] AS $oneRow) {
+	$str = trim($oneRow);
+	$result = preg_match_all(
+		/** @lang text */
+		"/<td[^>]*>(\n?\r?.*?)<\/td>/s", $str, $tdMatches);
+	$counter = 0;
+	foreach ((array)$tdMatches[1] AS $oneColumn) {
+		if (array_key_exists($counter, $neededColumns)) {
+			$columnName = $neededColumns[$counter];
+			$columnText = str_replace(',', '.', trim(strip_tags($oneColumn)));
+			if ($counter === 4) {
+				$estimate = strpos($columnText, ' П') !== FALSE;
+				$resultsArr[$resultsCounter]['Estimate'] = $estimate;
+				list ($day, $month, $year) = explode('.', substr($columnText, 0, 10));
+//				$columnText = $year . '-' . $month . '-' . $day;
+				$columnText = $day . '/' . $month .'/' . $year;
+			}
+			$resultsArr[$resultsCounter][$columnName] = $columnText;
+		}
+		$counter++;
+	}
+	$resultsCounter++;
+}
+
+if ($debugInfo) {
+	$s = !\Kint::dump(microtime(), 'finished preparing results array');
+}
+
+echo json_encode($resultsArr);
+
+/*$newHTML = str_replace('REPLACE_ME', $rowsStr, $newHTML);
+
+$document = new DOMDocument();
+$document->loadHTML($newHTML);
+$rows = $document->getElementsByTagName('tr');
+foreach ($rows AS $oneRow) {
+
+	ddd ( $oneRow->nodeValue );
+}
+$count = $matches->length;
+
+ddd ( $count );*/
