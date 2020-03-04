be5942e606cafcdc55a53f205c1ead199cd60958
Svaroggg <svaroggg@gmail.com>
2020-03-04 23:54:19 +0300

    Parse XML with payments. Done parsing and output into a table

--- src/SVApp/Controllers/ExpensesControllerProvider.php
+++ src/SVApp/Controllers/ExpensesControllerProvider.php
@@ -36,24 +36,52 @@ class ExpensesControllerProvider implements ControllerProviderInterface {
 		foreach ($expenseFiles AS $oneFile) {
 			$basename   = basename($oneFile);
 			$expenses[] = [ 'name' => $basename,
-			                'url'  => str_replace('.xml', '', $basename),
-			                'date' => date('Y-m-d H:i', filemtime($oneFile)), ];
+							'url'  => str_replace('.xml', '', $basename),
+							'date' => date('Y-m-d H:i', filemtime($oneFile)), ];
 		}
 
 
 		return $app['twig']->render('expenses.html.twig',
-		                            [ 'expenses' => $expenses, ]);
+									[ 'expenses' => $expenses, ]);
 	}
 
 	public function showExpenses($fileName) {
-		$fileName   = preg_replace('/[^a-z_]/i', '', $fileName);
-		$pathToFile = $this->app['files_dir'] . '/xmls/' . $fileName . '.xml';
+		$fileName   = preg_replace('/[^a-z_0-9]/i', '', $fileName) . '.xml';
+		$pathToFile = $this->app['files_dir'] . '/xmls/' . $fileName;
 		$content    = file_get_contents($pathToFile);
 
 		$parsedArr = simplexml_load_string($content);
 
-		ddd($parsedArr->Transactions);
+		$totalSumRub = 0;
 
-		return TRUE;
+		$cleanedTransactions = [];
+		$dateStart           = (string)$parsedArr->BeginDate;
+		$dateEnd             = (string)$parsedArr->EndDate;
+		foreach ($parsedArr->Transactions[0] AS $oneTransaction) {
+			$cleanTransaction = [ 'CardNumber'      => substr((string)$oneTransaction->Object, -4),
+								  'OperationDate'   => (string)$oneTransaction->TransactionDate,
+								  'ProcessDate'     => (string)$oneTransaction->ProcessedDate,
+								  'SumInSourceCur'  => $this->cleanAmount($oneTransaction->TransactionSum),
+								  'Currency'        => (string)$oneTransaction->TransactionCurrency,
+								  'SumInRub'        => $this->cleanAmount($oneTransaction->SumInAccountCurrency),
+								  'AccountCurrency' => (string)$oneTransaction->AccountCurrency,
+								  'Place'           => (string)$oneTransaction->Details,
+								  'Status'          => (string)$oneTransaction->Статус, ];
+			$totalSumRub      += $cleanTransaction['SumInRub'];
+
+			$cleanedTransactions[] = $cleanTransaction;
+		}
+
+
+		return $this->app['twig']->render('one_expense.html.twig',
+										  [ 'expenses'    => $cleanedTransactions,
+											'fileName'    => $fileName,
+											'dateStart'   => $dateStart,
+											'dateEnd'     => $dateEnd,
+											'totalSumRub' => number_format($totalSumRub, 2, '.', '') ]);
+	}
+
+	private function cleanAmount($amount) {
+		return str_replace([ ',', ' ' ], [ '.', '' ], (string)$amount);
 	}
 }
